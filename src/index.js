ac1a9caf28bc2f11fcaa8ec01e9b0cc8f9845798
isaque souza ramos <isaquesouzaramos@gmail.com>
2020-07-29 19:29:08 -0300

    Aula2-imersao-react-alura

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
+import CadastroVideo from './pages/cadastro/Video'
+
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/" component={Home} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
